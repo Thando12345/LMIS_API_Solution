2287c7836326f03500e384104aeaac0745da6f69
Thando12345 <69775289+Thando12345@users.noreply.github.com>
2024-11-06 17:55:46 +0200

    ui

--- RestFull_API/obj/Debug/net8.0/StockSense_DotNET_API.AssemblyInfo.cs
+++ RestFull_API/obj/Debug/net8.0/StockSense_DotNET_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockSense_DotNET_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d3f056d8bcfdbdc6372b2cf6cd8f54bed74df0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a4c0ddb027c74c17b7ea17f22e17e1aeccb1d07")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockSense_DotNET_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockSense_DotNET_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
