8e03cff5178d2a2dd01963f62a45fcc016ee95d0
Thando12345 <69775289+Thando12345@users.noreply.github.com>
2025-01-17 09:54:36 +0200

    UI and Logic

--- LMIS_Dev_Branch/obj/Debug/net8.0-windows/LMIS_Dev_Branch.AssemblyInfo.cs
+++ LMIS_Dev_Branch/obj/Debug/net8.0-windows/LMIS_Dev_Branch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMIS_Dev_Branch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac7ee06f0394e4f85ad5ef8763c34fd5b57ae1b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7789968056f945e2807c3d8bf1fe7994053282f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMIS_Dev_Branch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMIS_Dev_Branch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
