f64468d93306dc0a509877b73e7086a6dff75b25
Thando12345 <69775289+Thando12345@users.noreply.github.com>
2025-01-20 09:08:23 +0200

    UI and Logic

--- LMIS_Dev_Branch/obj/Debug/net8.0-windows/LMIS_Dev_Branch.AssemblyInfo.cs
+++ LMIS_Dev_Branch/obj/Debug/net8.0-windows/LMIS_Dev_Branch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMIS_Dev_Branch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0029a3bfa8f0148adceeb52a984dc421a5e8384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+037f24e92bca682a030ffbe9046b071a7623e589")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMIS_Dev_Branch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMIS_Dev_Branch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RestFull_API/obj/Debug/net8.0-windows7.0/StockSense_DotNET_API.AssemblyInfo.cs
+++ RestFull_API/obj/Debug/net8.0-windows7.0/StockSense_DotNET_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockSense_DotNET_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0029a3bfa8f0148adceeb52a984dc421a5e8384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+037f24e92bca682a030ffbe9046b071a7623e589")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockSense_DotNET_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockSense_DotNET_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
